6d81343e7d0b822e3cdba26e452927e4577d429e
bombrary <bombrary@users.noreply.github.com>
2022-12-02 02:12:38 +0000

    deploy: cf4b1afcfc09607817600cd7c358336383f77dbb

---
+++ works/sea-of-characters/script.js
@@ -0,0 +1,93 @@
+let width, height;
+let entities;
+const [fontSizeMin, fontSizeMax] = [0, 100];
+const [rMin, rMax] = [0, 100];
+const [textDxMin, textDxMax] = [-50, 50];
+const [textDyMin, textDyMax] = [-1, 0];
+const [circleDxMin, circleDxMax] = [-0.5, 0.5];
+const [circleDyMin, circleDyMax] = [-1, 0];
+
+
+function setup() {
+    width = windowWidth;
+    height = windowHeight;
+    createCanvas(width, height);
+    smooth();
+
+    entities = [...createTexts(20), ...createCircles(20)];
+}
+
+
+function draw() {
+    background('#181B39');
+    for (const e of entities) {
+      e.move();
+      e.draw();
+    }
+}
+
+
+const createCircles = (n) => 
+  Array.from({ length: n })
+    .map(() => {
+      const pos = createVector(random(0, width), random(0, height));
+      const r = random(rMin, rMax);
+      return new Circle(pos, r)
+    });
+
+
+const createTexts = (n) => 
+  Array.from({ length: n })
+    .map(() => {
+      const pos = createVector(random(0, width), random(0, height));
+      const size = random(fontSizeMin, fontSizeMax);
+      return new Text(pos, size);
+    });
+
+
+class Text {
+  constructor(pos, size) {
+    this.pos = pos;
+    this.size = size;
+  }
+  move() {
+    this.pos.add(random(textDxMin, textDxMax), random(textDyMin, textDyMax));
+    if (this.pos.y < -this.size) {
+      this.pos = createVector(random(0, width), height + this.size);
+    }
+  }
+  draw() {
+    noFill();
+    stroke(255);
+
+    const str = randomString(5);
+    textSize(this.size);
+    text(str, this.pos.x, this.pos.y);
+  }
+}
+
+
+class Circle {
+  constructor(pos, r) {
+    this.pos = pos;
+    this.r = r
+  }
+  move() {
+    this.pos.add(random(circleDxMin, circleDxMax), random(circleDyMin, circleDyMax));
+    if (this.pos.y < -this.r) {
+      this.pos = createVector(random(0, width), height + this.r);
+    }
+  }
+  draw() {
+    noFill();
+    stroke(255);
+
+    ellipse(this.pos.x, this.pos.y, this.r);
+  }
+}
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split('')
+const randomString = (n) =>
+  Array.from({ length: n })
+    .map(() => random(characters))
+    .join('')
